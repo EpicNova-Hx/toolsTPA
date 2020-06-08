9f222057fd6cde9be0e25682e1a2f1b4434c7430
EpicNova-Hx <66078147+EpicNova-Hx@users.noreply.github.com>
2020-06-08 20:39:09 +0700

    Add files via upload

---
+++ TPAV2.sh
@@ -0,0 +1,174 @@
+#batas gan
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+figlet "Halo"
+echo $pur "Elite"$ku"V2"
+echo $cy"-----------------"$pur"-------------------"
+echo $cy" Author : Manca"
+echo $cy" Youtube : EpicNo"$pur"va"
+echo $cy" Whatsapp : 08880"$pur"2118339"
+echo $cy"-----------------"$pur"-------------------" 
+sleep 2
+echo $bi "Pilih" $i "Yang" $cy "Mana" $pu "[?]"
+sleep 1
+echo [1]Clear
+echo [2]login termux
+echo [3]buka aplikasi Youtube
+echo [4]buka aplikasi google
+echo [5]membuat text ascii
+echo [6]membuka akun github anda
+echo [7]akun github admin
+echo [8]download lagu Cartoon OnOn Ncs Release
+echo [9]download lagu Warrior-Mortals Ncs
+echo [10]download dj Pinjam Barang
+echo [11]download kinemaster diamond
+echo [12]download kinemaster ruby
+echo [13]tonton video script TPA V1
+echo [14]download lagu DJ Bad Liar
+echo [15]download lagu Bad Liar
+echo [16]download lagu marshmellow alone
+echo [17]downlpad lagu electro light symbolism NCS
+read -p "[?] Choice : " pil :
+#Batas Gan
+if [ $pil = 1 ]
+then
+clear
+sleep 1
+figlet "cleared"
+sleep 1
+echo "Terimakasih Telah Mencoba Script Kami"
+echo "Jangan Lupa Subscribe Channel EpicNova"
+sleep 1
+echo $ku "Byee"
+fi
+#batas gan
+if [ $pil = 2 ]
+then
+clear
+login
+sleep 1
+figlet "logged"
+sleep 2
+echo "terimakasih telah mencoba script kami"
+sleep 1
+echo "jangan lupa subscribe channel epicnova"
+fi
+#batas gan
+if [ $pil = 3 ]
+then
+xdg-open https://youtube.com
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 4 ]
+then
+xdg-open https://google.com
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 5 ]
+then
+xdg-open http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+sleep 2
+sh TPAV2.sh
+fi   
+#batas gan
+if [ $pil = 6 ]
+then
+xdg-open https://github.com
+sleep 2
+sh TPAV2.sh
+fi   
+#batas gan
+if [ $pil = 7 ]
+then
+xdg-open https://github.com/EpicNova-Hx
+sleep 2
+sh TPAV2.sh
+fi   
+#batas gan
+if [ $pil = 8 ]
+then
+xdg-open https://tif.fjrifj.frl/e852cb92ab270e51fa61614d45ab6f75/K4DyBUG242c
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 9 ] 
+then
+xdg-open https://tif.fjrifj.frl/e852cb92ab270e51fa61614d45ab6f75/K4DyBUG242c
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 10 ]
+then
+xdg-open https://rjl.fjrifj.frl/8370bb2539450f352da65f5cb7318750/Tr1eTZRb6gI
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 11 ]
+then
+xdg-open https://www.mediafire.com/download/9p8wsnwupnq0lun
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 12 ]
+then
+xdg-open https://www.mediafire.com/download/6b2wa08cmtsr8x8
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 13 ]
+then
+xdg-open https://youtu.be/98VOF01rtLg
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 14 ]
+then
+xdg-open https://fff.fjrifj.frl/69308194c4e64031b734332f22e3fb84/6nk7qiKWR9Q
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 15 ]
+then
+xdg-open https://ijt.fjrifj.frl/907b3f6b781a3d75c818174c9c3d26e1/I-QfPUz1es8
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 16 ]
+then
+xdg-open https://frr.fjrifj.frl/3f7e2ff53298bd8011bfe9cf8e6e877b/ALZHF5UqnU4
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 17 ]
+then
+xdg-open https://ljt.fjrifj.frl/d1bbecbcccdd05d13e8a10927435571b/__CRWE-L45k
+sleep 2
+sh TPAV2.sh
+fi
+#batas gan
+if [ $pil = 18 ]
+then
+xdg-open https://rjj.fjrifj.frl/2f2c82f2739bda1d33e7f08a0271a0cf/J2X5mJ3HDYE
+sleep 2
+sh TPAV2.sh
+fi
+                     
